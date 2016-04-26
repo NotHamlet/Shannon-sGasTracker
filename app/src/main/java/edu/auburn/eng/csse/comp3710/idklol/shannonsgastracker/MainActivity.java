6f45297d4205bcf2968f82e2277ab7a7c6a62d5e
stephen <stephen.a.atwood@gmail.com>
2016-04-25 21:43:20 -0500

    Initial Commit

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/idklol/shannonsgastracker/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.auburn.eng.csse.comp3710.idklol.shannonsgastracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
