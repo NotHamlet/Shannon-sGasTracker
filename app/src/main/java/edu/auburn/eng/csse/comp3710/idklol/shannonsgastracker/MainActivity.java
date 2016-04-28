df7d94bb046609f99bed56f95d59a7ad0521aa8f
stephen <stephen.a.atwood@gmail.com>
2016-04-27 20:06:27 -0500

    Added Gas Record button to actionable

--- app/src/main/java/edu/auburn/eng/csse/comp3710/idklol/shannonsgastracker/MainActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/idklol/shannonsgastracker/MainActivity.java
@@ -3,6 +3,8 @@ package edu.auburn.eng.csse.comp3710.idklol.shannonsgastracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
